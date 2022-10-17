c4356f41882adccbe0c54faacee54445d749618e
Konstantin Lebedkov <lebedkov.k@gmail.com>
2022-10-17 18:29:08 +0700

    one bugfix a few comments

--- qsp/qsp_actions.cpp
+++ qsp/qsp_actions.cpp
@@ -26,10 +26,9 @@ std::vector<qsp_action*>::iterator qsp_actions::ActIndex(qsp_string name)
 }
 
 void qsp_actions::AddAction(std::vector<qsp_variant*> args, qsp_LinesOfCode* code, int start, int end)
-// вектор аргументов, каунт (набуа нужен? неясно. есть сайз вектора аргументов), код, начальный и конечный элемент которые надо взять из кода.
-// еще вопрос - откель берутся переменные qspRealCurLoc и qspRealActIndex. это типа глобальные переменные, и получать их надо через вызов? или через ссыль на овнер? тогда нужен интерфейс к этому овнеру.
+// вектор аргументов, код, начальный и конечный элемент которые надо взять из кода.
 {
-    if (ActIndex((args[0]).Val.Str) != CurActs.end()) return; //Check vector of actions for action named like this. If found, stop adding
+    if (ActIndex(*args[0]->Val.Str) != CurActs.end()) return; //Check vector of actions for action named like this. If found, stop adding
     if (CurActs.size() >= QSP_MAXACTIONS) { qspSetError(QSP_ERR_CANTADDACTION); return; }//Check amount of actions. if Limit achieved, stop and set error.
     CurActs.push_back(new qsp_action(args, code, start, end));
     IsActionsChanged = true;
