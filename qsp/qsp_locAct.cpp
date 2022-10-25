cd5ee979656bbe1f54261883e75843e900611732
Konstantin Lebedkov <lebedkov.k@gmail.com>
2022-10-25 15:08:26 +0700

    лок-акшен вынесен в отдельный класс

---
+++ qsp/qsp_locAct.cpp
@@ -0,0 +1,1 @@
+#include "qsp_locAct.h"
---
+++ qsp/qsp_locAct.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "qsp_string.h"
+
+class qsp_locAct
+{
+    String Image;
+    String Desc;
+    QSPLineOfCode* OnPressLines;
+    int OnPressLinesCount;
+};
+
